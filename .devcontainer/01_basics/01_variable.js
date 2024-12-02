67610a05848fad4b7aab53f9c43a0cd313b45fcf
chirag03200 <jainc3620@gmail.com>
2024-12-02 11:34:35 +0000

    container config

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId= 14356
+let accountEmail="jainc3620@gmail.com"
+var accountPassword= "12345"
+accountCity="jaipur"
+let accountState;
+
+// accountId= 2 ; not allwoed
+accountEmail= "abcd@gmail.com"
+accountPassword="231231"
+accountCity= "ajmer"
+
+/* 
+prefer the  let only instead of  var*/
+
+console.log(accountId);
+console.table([accountPassword,accountEmail, accountId, accountCity, accountState])
