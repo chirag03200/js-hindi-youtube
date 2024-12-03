31fbdf0b38f689466f9a53893b6bceae0fa4da8d
chirag03200 <jainc3620@gmail.com>
2024-12-03 09:16:04 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) we are using ndejs , not browser
+
+console.log(3+3);
+console.log("chirag");
+
+/* PRIMITIVE DATA TYPES-:
+   range of number datatype is 2 to the power 53
+   bigInt=> use for larger numbers like on facebook etc.
+   string=>"" and '' but i prefer use only "".
+   boolean=> true and false
+   null=> standalone value => representation of empty value
+   undefined=>the value is not define
+   symbol=> use for unique blocks and component
+
+   object=> it's use by type of null */
+
+console.log(typeof undefined); // ressult is show undefined
+console.log(typeof null); // ressult is show object
+console.log(typeof 42); //  ressult is show number
+console.log(typeof "CHIRAG"); //  ressult is show string
+
+
+
+
+
